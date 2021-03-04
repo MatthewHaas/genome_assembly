3113cdc6549761204fa211617bb930e8adf0f120
Matthew Haas <matthew.haas1@gmail.com>
2021-03-04 11:10:41 -0600

    Commit

---
+++ downsampling_scripts/filter_with_vcftools_8fold.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=32
+#SBATCH --time=2:00:00
+#SBATCH --mem=30g
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=haasx092@umn.edu
+#SBATCH -p amdsmall
+#SBATCH --account=jkimball
+#SBATCH -o filter_vcfs_8fold.out
+#SBATCH -e filter_vcfs_8fold.err
+
+# Include path to the working directory
+cd ~/pilot_GBS/200519_downsampled_8fold
+
+# Filter VCF files to keep samples with fewer than 200 genotypes missing (to make a reference panel)
+for i in $(cat vcf_list_new.txt);
+do
+STEM=$(echo ${i} | cut -f 1 -d ".")
+~/vcftools/bin/vcftools --gzvcf  $i --max-missing 0.9 --min-alleles 2 --max-alleles 2 --remove-indels --minDP 6 --recode --recode-INFO-all --out ${STEM}_filtered
+done
