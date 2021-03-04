22d9cb0d665da5aab23e8b6c3466852adf3e1d2a
Matthew Haas <matthew.haas1@gmail.com>
2021-03-04 10:11:03 -0600

    Label axes in outer margins and change font to serif

--- assorted_scripts/pilot_GBS_plot_SNP_distr.R
+++ assorted_scripts/pilot_GBS_plot_SNP_distr.R
@@ -11,14 +11,18 @@ pdf("210302_snp_distribution.pdf")
 layout(matrix(c(1,5,9,13,17,
                 2,6,10,14,0,
                 3,7,11,15,0,
-                4,8,12,16,0), nrow=5), widths=c(1,1))
+                4,8,12,16,0), nrow=5), widths=c(1,1,1,1))
+par(oma=c(3,6,3,0))
 par(mar=c(2,2,2,2))
 for (i in scaffolds){
   y <- x[scaffold == i, .N, key=.(bin=position %/% 1e6)]
   y[, plot(x = bin, y = N, main = i,
            xlab = "chromosome position (Mb)",
            ylab = "# of SNPs",
+           family = "serif",
            type = "l",
            las = 1)]
+ mtext(text = "Number of SNPs", family = "serif", side = 2, line = 1, outer = TRUE)  
+ mtext(text = "Chromosome position (Mb)", family = "serif", side = 1, outer = TRUE)
 }
 dev.off()
